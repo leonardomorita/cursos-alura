1a0041525b52e72adbad80e8c55dd683e5b589bd
leonardomorita <leonardomorita2@gmail.com>
2024-05-22 23:36:16 -0300

    ✨ Aula 02 - Catálogo de filmes Parte 01 - Atribuindo valores ao filme.

---
+++ cursos-php/criando-sua-aplicacao/02-screen-match.php
@@ -0,0 +1,13 @@
+<?php
+
+echo 'Bem-vindo(a) ao Screen Match!' . PHP_EOL;
+
+$nomeFilme = 'Top Gun - Maverick';
+$anoLancamento = 2022;
+$notaFilme = 8.8;
+$incluidoNoPlano = true;
+
+echo "Filme: $nomeFilme" . PHP_EOL;
+echo "Lançamento: $anoLancamento" . PHP_EOL;
+echo "Nota: $notaFilme" . PHP_EOL;
+echo "Incluso no Plano: $incluidoNoPlano" . PHP_EOL;
