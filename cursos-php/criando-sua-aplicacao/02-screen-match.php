5a4f02308e4eb45e2cb9bac0c5f92fbc3d0a3882
leonardomorita <leonardomorita2@gmail.com>
2024-05-22 23:50:23 -0300

    ✨ Aula 02 - Catálogo de filmes Parte 02 - Operando sobre dados.

--- cursos-php/criando-sua-aplicacao/02-screen-match.php
+++ cursos-php/criando-sua-aplicacao/02-screen-match.php
@@ -4,10 +4,11 @@ echo 'Bem-vindo(a) ao Screen Match!' . PHP_EOL;
 
 $nomeFilme = 'Top Gun - Maverick';
 $anoLancamento = 2022;
-$notaFilme = 8.8;
-$incluidoNoPlano = true;
+$notaFilme = (9 + 6 + 8 + 7.5 + 5) / 5;
+$planoPrime = true;
+$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 
 echo "Filme: $nomeFilme" . PHP_EOL;
 echo "Lançamento: $anoLancamento" . PHP_EOL;
 echo "Nota: $notaFilme" . PHP_EOL;
-echo "Incluso no Plano: $incluidoNoPlano" . PHP_EOL;
+echo "Incluso no Plano: " . var_export($incluidoNoPlano, true) . PHP_EOL;
