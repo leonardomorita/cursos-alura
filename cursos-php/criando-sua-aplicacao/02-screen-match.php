6c023042170baf852c3bd3e5dcbb4f77fefdf4d2
leonardomorita <leonardomorita2@gmail.com>
2024-05-25 01:15:57 -0300

    ✨ Aula 02 - Catálogo de filmes Parte 07 - Entrada de dados - $argsv e operador ??.

--- cursos-php/criando-sua-aplicacao/02-screen-match.php
+++ cursos-php/criando-sua-aplicacao/02-screen-match.php
@@ -3,7 +3,9 @@
 echo 'Bem-vindo(a) ao Screen Match!' . PHP_EOL;
 
 $nomeFilme = 'Top Gun - Maverick';
-$anoLancamento = 2022;
+// $argv => Obtém todas entradas que forem passadas no terminal.
+// ?? => Operador coalescência nula. Se o valor que está à esquerda desse operador for nulo, o que vem à direita vai ser utilizado.
+$anoLancamento = $argv[1] ?? 2022;
 $notaFilme = (9 + 6 + 8 + 7.5 + 5) / 5;
 $planoPrime = true;
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
