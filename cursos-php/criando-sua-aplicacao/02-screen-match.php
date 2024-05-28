c19d0b62e0f274d5eed07c38cad01ef760223f71
leonardomorita <leonardomorita2@gmail.com>
2024-05-28 07:51:49 -0300

    ✨ Aula 03 - Controlando o fluxo Parte 03 - Expressão condicional.

--- cursos-php/criando-sua-aplicacao/02-screen-match.php
+++ cursos-php/criando-sua-aplicacao/02-screen-match.php
@@ -3,9 +3,13 @@
 echo 'Bem-vindo(a) ao Screen Match!' . PHP_EOL;
 
 $nomeFilme = 'Top Gun - Maverick';
+$nomeFilme = 'Thor: Ragnarok';
+$nomeFilme = 'Se beber não case';
+
 // $argv => Obtém todas entradas que forem passadas no terminal.
 // ?? => Operador coalescência nula. Se o valor que está à esquerda desse operador for nulo, o que vem à direita vai ser utilizado.
 $anoLancamento = $argv[1] ?? 2022;
+
 $notaFilme = (9 + 6 + 8 + 7.5 + 5) / 5;
 $planoPrime = true;
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
@@ -22,3 +26,15 @@ if ($anoLancamento > 2023) {
 } else {
     echo "Esse filme não é um lançamento." . PHP_EOL;
 }
+
+// Uma expressão é tudo que depois de processado torna-se um valor que pode ser atribuído à uma variável.
+// Um ponto e vírgula sempre marca o final de uma expressão.
+// match é uma expressão (match expression).
+$genero = match ($nomeFilme) {
+    'Top Gun - Maverick' => 'ação',
+    'Thor: Ragnarok' => 'super-herói',
+    'Se beber não case' => 'comédia',
+    default => 'desconhecido'
+};
+
+echo "O gênero do filme é: $genero" . PHP_EOL;
