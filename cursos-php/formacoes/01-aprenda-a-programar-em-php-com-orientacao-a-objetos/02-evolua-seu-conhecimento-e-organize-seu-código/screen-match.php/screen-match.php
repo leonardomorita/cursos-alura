f61879d88fc94361e66d7083ddc8a3132523b69f
leonardomorita <leonardomorita2@gmail.com>
2024-06-02 13:06:02 -0300

    ✨ Aula 02 - Usando a linguagem Parte 08 - Separando em arquivos.

--- cursos-php/formacoes/01-aprenda-a-programar-em-php-com-orientacao-a-objetos/02-evolua-seu-conhecimento-e-organize-seu-código/screen-match.php/screen-match.php
+++ cursos-php/formacoes/01-aprenda-a-programar-em-php-com-orientacao-a-objetos/02-evolua-seu-conhecimento-e-organize-seu-código/screen-match.php/screen-match.php
@@ -1,20 +1,6 @@
 <?php
 
-function exibirMensagemLancamento(int $ano): void
-{
-    if ($ano > 2023) {
-        echo "Esse filme é um lançamento." . PHP_EOL;
-    } elseif ($ano > 2021 && $ano <= 2023) {
-        echo "Esse filme ainda é novo." . PHP_EOL;
-    } else {
-        echo "Esse filme não é um lançamento." . PHP_EOL;
-    }
-}
-
-function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool
-{
-    return $planoPrime || $anoLancamento < 2020;
-}
+require __DIR__ . '/funcoes.php';
 
 echo 'Bem-vindo(a) ao Screen Match!' . PHP_EOL;
 
