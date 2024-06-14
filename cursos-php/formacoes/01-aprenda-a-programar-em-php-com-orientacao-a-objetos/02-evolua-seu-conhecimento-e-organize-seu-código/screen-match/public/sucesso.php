ca58b91cd2244997710ce0a41c6ffe2264299200
leonardomorita <leonardomorita2@gmail.com>
2024-06-14 07:45:23 -0300

    ✨ Aula 5 - PHP na web Parte 09 - Lista de exercícios.

--- cursos-php/formacoes/01-aprenda-a-programar-em-php-com-orientacao-a-objetos/02-evolua-seu-conhecimento-e-organize-seu-código/screen-match/public/sucesso.php
+++ cursos-php/formacoes/01-aprenda-a-programar-em-php-com-orientacao-a-objetos/02-evolua-seu-conhecimento-e-organize-seu-código/screen-match/public/sucesso.php
@@ -1,1 +1,11 @@
+<?php
+$handle = fopen(__DIR__ . '/filme.json', 'r');
+$filmeJson = fgets($handle);
+fclose($handle);
+
+$filme = json_decode($filmeJson, true);
+?>
 <h1>Sucesso! Filme <?= $_GET['filme']; ?> inserido.</h1>
+<h2>Ano de Lançamento: <?= $filme['anoLancamento']; ?></h2>
+<h2>Nota: <?= $filme['nota']; ?></h2>
+<h2>Gênero: <?= $filme['genero']; ?></h2>
