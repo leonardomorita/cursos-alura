cb67c799d80abcaa209132dccf42b1f37d6701ea
leonardomorita <leonardomorita2@gmail.com>
2024-07-06 18:15:30 -0300

    ✨ Aula 03 - Modelo mais robusto Parte 06 - Definindo um tipo para gênero.

--- cursos-php/formacoes/01-aprenda-a-programar-em-php-com-orientacao-a-objetos/03-conheca-a-programacao-orientada-a-objetos/index.php
+++ cursos-php/formacoes/01-aprenda-a-programar-em-php-com-orientacao-a-objetos/03-conheca-a-programacao-orientada-a-objetos/index.php
@@ -1,10 +1,11 @@
 <?php
 
+require __DIR__ . '/src/Modelo/Genero.php';
 require __DIR__ . '/src/Modelo/Filme.php';
 
 echo 'Bem-vindo(a) ao Screen Match!' . PHP_EOL;
 
-$filme = new Filme('Thor - Ragnarok', 2021, 'super-heroi');
+$filme = new Filme('Thor - Ragnarok', 2021, Genero::SuperHeroi);
 
 $filme->avalia(10);
 $filme->avalia(6);
@@ -13,5 +14,5 @@ $filme->avalia(8.2);
 
 var_dump("Nome: $filme->nome");
 var_dump("Ano do Lançamento: $filme->anoLancamento");
-var_dump("Gênero: $filme->genero");
+var_dump("Gênero: {$filme->genero->name}");
 var_dump("Média de notas: {$filme->mediaNota()}");
---
+++ cursos-php/formacoes/01-aprenda-a-programar-em-php-com-orientacao-a-objetos/03-conheca-a-programacao-orientada-a-objetos/src/Modelo/Genero.php
@@ -0,0 +1,12 @@
+<?php
+
+// Enum: é um tipo que permite enumerar um conjunto de valores
+
+enum Genero
+{
+    case Acao;
+    case Comedia;
+    case Terror;
+    case SuperHeroi;
+    case Drama;
+}
