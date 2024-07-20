8cd1edcf52f831d1b66ca5117b5875dab1c2a3ab
leonardomorita <leonardomorita2@gmail.com>
2024-07-19 23:51:57 -0300

    ✨ Aula 04 - Lidando com falta de notas Parte 06 - Lidando com múltiplos tipos.

--- cursos-php/formacoes/01-aprenda-a-programar-em-php-com-orientacao-a-objetos/04-use-interfaces-namespaces-traits-e-excecoes/src/Calculos/ConversorNotaEstrela.php
+++ cursos-php/formacoes/01-aprenda-a-programar-em-php-com-orientacao-a-objetos/04-use-interfaces-namespaces-traits-e-excecoes/src/Calculos/ConversorNotaEstrela.php
@@ -4,6 +4,7 @@ namespace ScreenMatch\Calculos;
 
 use DivisionByZeroError;
 use ScreenMatch\Modelo\Avaliavel;
+use Throwable;
 
 class ConversorNotaEstrela
 {
@@ -14,6 +15,9 @@ class ConversorNotaEstrela
     
             return round($nota) / 2;
         } catch (DivisionByZeroError $divisionByZeroError) {
+            return 0;
+        } catch (Throwable $throwable) {
+            var_dump($throwable->getMessage());
             return 0;
         }
     }
