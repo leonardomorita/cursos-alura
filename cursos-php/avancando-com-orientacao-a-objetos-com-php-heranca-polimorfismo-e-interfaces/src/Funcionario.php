eba83f85ef39f1861f3ac9dab17edfa7e4f704d5
leonardomorita <leonardomorita2@gmail.com>
2024-08-01 21:16:32 -0300

    ✨ Aula 01 - Herança Parte 05, 07 e 09 - A Classe Funcionário, Usando herança e Parent e protected.

---
+++ cursos-php/avancando-com-orientacao-a-objetos-com-php-heranca-polimorfismo-e-interfaces/src/Funcionario.php
@@ -0,0 +1,17 @@
+<?php
+
+class Funcionario extends Pessoa
+{
+    private string $cargo;
+
+    public function __construct(string $nome, CPF $cpf, string $cargo)
+    {
+        parent::__construct($nome, $cpf);
+        $this->cargo = $cargo;
+    }
+
+    public function recuperaCargo(): string
+    {
+        return $this->cargo;
+    }
+}
---
+++ cursos-php/avancando-com-orientacao-a-objetos-com-php-heranca-polimorfismo-e-interfaces/src/Pessoa.php
@@ -0,0 +1,32 @@
+<?php
+
+class Pessoa
+{
+    private $nome;
+    private $cpf;
+
+    public function __construct(string $nome, CPF $cpf)
+    {
+        $this->validaNome($nome);
+        $this->nome = $nome;
+        $this->cpf = $cpf;
+    }
+
+    public function recuperaNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function recuperaCpf(): string
+    {
+        return $this->cpf->recuperaNumero();
+    }
+
+    private function validaNome(string $nome): void
+    {
+        if (strlen($nome) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit();
+        }
+    }
+}
