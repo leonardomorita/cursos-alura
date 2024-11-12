d15c4e08ede7c3a164c63147910b4400f4917d2d
leonardomorita <leonardomorita2@gmail.com>
2024-11-11 23:57:40 -0300

    ✨ Aula 05 - Models

--- cursos-php/formacoes/04-laravel-crie-aplicacoes-web-em-php/01-criando-uma-aplicacao-com-mvc/controle-series/app/Http/Controllers/SeriesController.php
+++ cursos-php/formacoes/04-laravel-crie-aplicacoes-web-em-php/01-criando-uma-aplicacao-com-mvc/controle-series/app/Http/Controllers/SeriesController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
     public function index()
     {
-        $series = [
-            "Punisher", "Lost", "Grey's Anatomy"
-        ];
+        // $series = Serie::all();
+        $series = Serie::query()->orderBy('nome', 'asc')->get();
+        // $series = DB::select('SELECT nome FROM series;');
 
         // return view('listar-series', compact('series'));
         return view('series.index')->with('series', $series);
@@ -19,5 +21,18 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+
+        // DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie]);
+
+        return redirect('/series');
     }
 }
--- cursos-php/formacoes/04-laravel-crie-aplicacoes-web-em-php/01-criando-uma-aplicacao-com-mvc/controle-series/resources/views/series/create.blade.php
+++ cursos-php/formacoes/04-laravel-crie-aplicacoes-web-em-php/01-criando-uma-aplicacao-com-mvc/controle-series/resources/views/series/create.blade.php
@@ -1,5 +1,7 @@
 <x-layout title="Nova Série">
-    <form action="" method="POST">
+    <form action="/series/salvar" method="POST">
+        @csrf
+
         <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
             <input type="text" class="form-control" id="nome" name="nome">
